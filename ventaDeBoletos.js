d7da50c955a2caf80d5ef668ba42f9beb002adae
Carolina <carolinadiaz.m539@gmail.com>
2023-09-15 08:42:16 -0300

    agrega archivos iniciales

---
+++ ventaDeBoletos.js
@@ -0,0 +1,35 @@
+let saludoUno = "¡Buenas tardes, ya tiene su boleto!" 
+let nombre = "Carolina ";
+let apellido = "Diaz";
+let edad = 28 + " años";
+let numeroTelefono = 1164862371;
+let esSocio = true;
+let fechaDelPartido = "15 de agosto";
+let numeroDeSilla = "nro " + 535;
+const precioDelBoleto = 9.599
+let saludoDos = "¡Que disfrute el partido!"
+
+
+console.log(saludoUno);
+  
+ 
+  console.log(nombre);
+    console.log(apellido);
+    console.log(edad);
+    console.log(numeroTelefono);
+    console.log(esSocio);
+    console.log(fechaDelPartido);
+    console.log(numeroDeSilla);
+    console.log("$" + precioDelBoleto);
+    console.log(saludoDos);
+
+    console.log(typeof saludoUno);
+    console.log(typeof nombre);
+    console.log(typeof apellido);
+    console.log(typeof numeroTelefono);
+    console.log(typeof edad);
+    console.log(typeof esSocio);
+    console.log(typeof fechaDelPartido);
+    console.log(typeof numeroDeSilla);
+    console.log(typeof precioDelBoleto);
+    console.log(typeof saludoDos);
